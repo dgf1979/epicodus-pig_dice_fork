485c287ee86188e560720f6aa5e52b9cc7008969
Paul Ogawa & Frank Johnson <student@epicodus.com>
2015-06-03 14:58:52 -0700

    is a finished product

--- js/scripts.js
+++ js/scripts.js
@@ -22,20 +22,32 @@ $(document).ready(function() {
     $("#add-player-1").hide();
     $("#display-name-1").html('<h2>' + playerOne.name + '</h2>');
     $("#player-1-shake").click(function(event) {
-      // var playerOne = new Player($(this).find("input#player-1-name"))
-      playerOne.roll();
-      $("#player-1-roll").html('<h4>' + playerOne.roll() + '</h4>');
-        if (playerOne.roll() < 2) {
+      var roll1 = playerOne.roll();
+      $("#player-1-roll").html('<h4>' + 'You rolled a: '  + roll1 + '</h4>');
+        if (roll1 < 2) {
           $("#player-1-shake").hide();
           $("#player-1-hold").hide();
           $("#player-2-shake").show();
           $("#player-2-hold").show();
           var score = playerOne.score;
-          $("#player-1-score").html('<h3>' + score + '</h3>');
+          $("#player-1-score").html('<h3>' + 'Your total score is: ' + score + '</h3>');
         } else {
           var score = playerOne.score;
-          $("#player12-score").html('<h3>' + score + '</h3>');
+          $("#player-1-score").html('<h3>' + 'Your total score is: '+ score + '</h3>');
         }
+        if (score >= 100) {
+          $("#player-2-shake").hide();
+          $("#player-2-hold").hide();
+          $("#player-1-shake").hide();
+          $("#player-1-hold").hide();
+          (alert("PLAYER ONE WINS MOFO, OINK OINK")) * 10;
+        }
+      });
+    $("#player-1-hold").click(function(event) {
+      $("#player-1-shake").hide();
+      $("#player-1-hold").hide();
+      $("#player-2-shake").show();
+      $("#player-2-hold").show();
     });
   });
 
@@ -46,20 +58,32 @@ $(document).ready(function() {
     $("#add-player-2").hide();
     $("#display-name-2").html('<h2>' + playerTwo.name + '</h2>');
     $("#player-2-shake").click(function(event) {
-      playerTwo.roll();
-      $("#player-2-roll").html('<h4>' + playerTwo.roll() + '</h4>');
-      if (playerTwo.roll() < 2) {
+      var roll2 = playerTwo.roll();
+      $("#player-2-roll").html('<h4>' + 'You rolled a: ' + roll2 + '</h4>');
+      if (roll2 < 2) {
         $("#player-2-shake").hide();
         $("#player-2-hold").hide();
         $("#player-1-shake").show();
-        $("#player-1-shake").show();
         $("#player-1-hold").show();
         var score = playerTwo.score;
-        $("#player-2-score").html('<h3>' + score + '</h3>');
+        $("#player-2-score").html('<h3>' + 'Your total score is: ' + score + '</h3>');
       } else {
         var score = playerTwo.score;
-        $("#player-2-score").html('<h3>' + score + '</h3>');
+        $("#player-2-score").html('<h3>'  + 'Your total score is: ' + score + '</h3>');
       }
+      if (score >= 100) {
+        $("#player-2-shake").hide();
+        $("#player-2-hold").hide();
+        $("#player-1-shake").hide();
+        $("#player-1-hold").hide();
+        alert("PLAYER TWO WINS MOFO, OINK OINK");
+      }
+    });
+    $("#player-2-hold").click(function(event) {
+      $("#player-2-shake").hide();
+      $("#player-2-hold").hide();
+      $("#player-1-shake").show();
+      $("#player-1-hold").show();
     });
   });
 });
